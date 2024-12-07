8e1b76d93525426da19d4cb1259aa7b99d4abc15
kimminjeong2 <zbdj0123@naver.com>
2024-12-07 09:37:00 +0900

    폰트

--- src/components/Mobile/Main/TeamContainer.tsx
+++ src/components/Mobile/Main/TeamContainer.tsx
@@ -6,7 +6,7 @@ const TeamContainer: React.FC<Props> = ({ color }) => {
   const textColor = color === "RED" ? "#8A2922" : "#004A82";
   return (
     <>
-      <p className="text-[30px]" style={{ color: textColor }}>
+      <p className="text-[30px] font-blackHanSans" style={{ color: textColor }}>
         {color} TEAM
       </p>
       <div className="bg-[#F0E6D2] border-2 border-[#C8AA6E] rounded-[15px] bg-opacity-15 w-[300px] min-h-[250px]"></div>
--- src/components/Mobile/chooseUser/UserContainer.tsx
+++ src/components/Mobile/chooseUser/UserContainer.tsx
@@ -5,7 +5,7 @@ const UserContainer = ({ children }: Props) => {
   return (
     <>
       <div className="w-[300px] bg-white border-2 border-[#C8AA6E] rounded-[15px] bg-opacity-15 mt-10">
-        <div className="flex flex-row items-center justify-center space-x-4 w-full mb-6 mt-5">
+        <div className="font-blackHanSans flex flex-row items-center justify-center space-x-4 w-full mb-6 mt-5">
           <button className="w-[18vw] h-[4.5vh] bg-[#F0E6D2] rounded-full border-2 border-[#C8AA6E] text-[9px] text-[#0F2041] flex items-center justify-center font-black">
             인원추가
           </button>
@@ -28,7 +28,7 @@ const UserContainer = ({ children }: Props) => {
         <UserInfo />
         {children}
       </div>
-      <button className="w-[18vw] h-[4.5vh] bg-[#F0E6D2] rounded-full border-2 border-[#C8AA6E] text-[9px] text-[#0F2041] flex items-center justify-center font-extrabold mt-7 mb-7">
+      <button className="font-blackHanSans w-[18vw] h-[4.5vh] bg-[#F0E6D2] rounded-full border-2 border-[#C8AA6E] text-[9px] text-[#0F2041] flex items-center justify-center font-extrabold mt-7 mb-7">
         팀짜기
       </button>
     </>
